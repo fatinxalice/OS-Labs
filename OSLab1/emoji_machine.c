a14cdd44ae078b26a0a3e558c1a116277782d240
fatinxalice <aisyah.fatin@gmail.com>
2020-04-02 15:25:24 +0800

    Emojis added

--- OSLab1/emoji_machine.c
+++ OSLab1/emoji_machine.c
@@ -4,7 +4,17 @@ int main(){
 
 char mood;
 
-printf("Choose your mood:\nA	Happy\nB	Sad\nC	Dead\n");
-scanf("%c", &mood); //code not done yet
+printf("Choose your mood:\nA	Happy\nB	Sad\nC	Overwhelmed\n");
+scanf("%c", &mood); 
+
+if(mood=='A'){
+ printf("Here's your emoji:\n:)\n");
+}
+else if(mood=='B'){
+ printf("Here's your emoji:\n:(\n");
+}
+else if(mood=='C'){
+ printf("Here's your emoji:\nO.o\n");
+}
 
 }
